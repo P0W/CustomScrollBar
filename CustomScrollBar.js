93b420e3fbb9556f694825e730b4e90f3c5be123
Prashant Srivastava <prashans@mathworks.com>
2020-11-19 18:39:39 +0530

    Updates for Click event - Smooth scroll

--- CustomScrollBar.js
+++ CustomScrollBar.js
@@ -1,3 +1,4 @@
+
 (function (scope) {
     scope.ScrollBar = function (config, callback, targetNode) {
         targetNode = targetNode || window;
@@ -28,12 +29,14 @@
 
         this.offset = offset; // 5px margin up and below
         this.height = targetNode.offsetHeight;
+        this.width = width;
 
-        // Considering it takes 20 wheels to move down
-        this.deltaMove = (this.height - thumbHeight - 2 * this.offset) / 20;
+        // Considering it takes 50 wheels to move down
+        this.deltaMove = (this.height - thumbHeight - this.offset) / 50;
         this.scrollDiv = scrollDiv;
         this.trackDiv = trackDiv;
         this.thumbDiv = thumbDiv;
+        this.targetNode = targetNode;
 
         // Congigure Track
         this.trackDiv.style.height = this.height + 'px';
@@ -55,10 +58,18 @@
         this.thumbDiv.addEventListener('mouseup',
             this.handleMouseUpEvt.bind(this));
 
-        // Configure events - On target node
-        targetNode.addEventListener('mousewheel',
+        // Configure events - On scrollbar main node
+        this.scrollDiv.addEventListener('mousewheel',
             this.handleMouseWheelEvt.bind(this));
-        targetNode.addEventListener('mouseup',
+
+        // Configure events - On track node
+        this.trackDiv.addEventListener('click',
+            this.handleMouseClickEvt.bind(this));
+
+        // Configure events - On target node
+        this.scrollDiv.addEventListener('mousewheel',
+            this.handleMouseWheelEvt.bind(this));
+        this.targetNode.addEventListener('mouseup',
             this.handleMouseUpEvt.bind(this));
 
         // Configure events - On document body
@@ -66,10 +77,6 @@
             this.handleMouseMoveEvt.bind(this));
         document.body.addEventListener('mouseup',
             this.handleMouseUpEvt.bind(this));
-
-        // Canvas Contexts
-        // this.trackCanvasCtx = this.trackDiv.getContext('2d');
-        // this.thumbCanvasCtx = this.thumbDiv.getContext('2d');
 
         // Scrollbar internal setup
         this.mouseDownStaus = {
@@ -80,16 +87,21 @@
 
         // The callback on scroll event
         this.callback = callback;
+        this.debouncedScroll = this.throttle(this.onScroll, 200);
     };
 
     scope.ScrollBar.prototype = {
 
         onScroll: function () {
-            this.callback(this.getThumbPosition() - this.offset);
+            if (this.isVisible()) {
+                this.callback(this.getThumbPosition() - this.offset);
+            }
         },
 
         onResize: function () {
-
+            this.height = this.targetNode.offsetHeight;
+            this.trackDiv.style.height = this.height + 'px';
+            this.scrollDiv.style.left = (this.targetNode.offsetWidth - this.width) + 'px';
         },
 
         setVisiblility: function (state) {
@@ -109,6 +121,10 @@
 
         getTopMargin: function () {
             return this.offset;
+        },
+
+        getSweepArea: function () {
+            return this.height - this.getTopMargin() - this.getThumbHeight();
         },
 
         setThumbHeight: function (size) {
@@ -164,6 +180,104 @@
                 this.onScroll();
                 this.mouseDownStaus.lastDragPos = this.getThumbPosition();
             }
+        },
+
+        handleMouseClickEvt: function (evt) {
+            if (this.isVisible()) {
+                var targetPos = evt.clientY - this.offset - this.getThumbHeight();
+                var currentPos = this.getThumbPosition();
+                var duration = 500;
+                var promise = this.soothScrollTo(currentPos, targetPos, duration);
+                // Invoke a final scroll
+                promise.then(() => this.onScroll());
+            }
+        },
+
+        soothScrollTo: function (startTop, target, duration) {
+            var startTime = Date.now();
+            var endTime = startTime + duration;
+            var distance = target - startTop;
+
+            var smoothStep = function (start, end, point) {
+                if (point <= start) {
+                    return 0;
+                }
+                if (point >= end) {
+                    return 1;
+                }
+                // interpolation
+                var x = (point - start) / (end - start);
+                return x * x * (3 - 2 * x);
+            };
+
+            return new Promise((resolve, reject) => {
+                var previousTop = startTop;
+                // This is like a think function from a game loop
+                var scrollFrame = function () {
+                    if (this.getThumbPosition() !== previousTop) {
+                        // eslint-disable-next-line prefer-promise-reject-errors
+                        reject('interrupted');
+                        return;
+                    }
+                    // set the scrollTop for this frame
+                    var now = Date.now();
+                    var point = smoothStep(startTime, endTime, now);
+                    var frameTop = Math.round(startTop + (distance * point));
+                    this.setThumbPosition(frameTop);
+                    this.onScroll();
+                    this.mouseDownStaus.lastDragPos = this.getThumbPosition();
+                    // check if we're done!
+                    if (now >= endTime) {
+                        resolve();
+                        return;
+                    }
+                    // If we were supposed to scroll but didn't, then we
+                    // probably hit the limit, so consider it done; not
+                    // interrupted.
+                    if (this.getThumbPosition() === previousTop &&
+                        this.getThumbPosition() !== frameTop) {
+                        resolve();
+                        return;
+                    }
+                    previousTop = this.getThumbPosition();
+                    window.requestAnimationFrame(scrollFrame);
+                }.bind(this);
+
+                window.requestAnimationFrame(scrollFrame);
+            });
+        },
+
+        debounce: function (func, wait) {
+            // Each call to the returned function will share this common timer.
+            var timeout;
+            // Calling debounce returns a new anonymous function
+            return function () {
+                // reference the context and args for the setTimeout function
+                var context = this;
+                var args = arguments;
+                //   Each time the returned function is called, the timer starts over.
+                clearTimeout(timeout);
+                // Set the new timeout
+                timeout = setTimeout(() => {
+                    // Inside the timeout function, clear the timeout variable
+                    timeout = null;
+                    // Call the original function with apply
+                    func.apply(context, args);
+                }, wait);
+            };
+        },
+
+        throttle: function (callback, limit) {
+            var waiting = false; // Initially, we're not waiting
+            return function () { // We return a throttled function
+                if (!waiting) { // If we're not waiting
+                    callback.apply(this, arguments); // Execute users function
+                    waiting = true; // Prevent future invocations
+                    setTimeout(function () { // After a period of time
+                        waiting = false; // And allow future invocations
+                    }, limit);
+                }
+            };
         }
     };
 })(this);
